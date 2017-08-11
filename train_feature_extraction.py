8b48eefddb8933cf51bc555d0a0bb1630b51dc52
HanByul Yang <hanbyul.yang@gmail.com>
2017-08-11 12:32:08 +0900

    14. Lab: Training the Feature Extractor

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -1,25 +1,83 @@
 import pickle
+import time
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 from alexnet import AlexNet
 
 # TODO: Load traffic signs data.
+nb_classes = 43
+EPOCHS = 10
+BATCH_SIZE = 128
+
+with open('train.p', 'rb') as f:
+    data = pickle.load(f)
 
 # TODO: Split data into training and validation sets.
+X_train, X_val, y_train, y_val = train_test_split(data['features'], data['labels'], test_size=0.33, random_state=0)
 
 # TODO: Define placeholders and resize operation.
+features = tf.placeholder(tf.float32, [None, 32, 32, 3],)
+labels = tf.placeholder(tf.int64, None)
+resized = tf.image.resize_images(features, [227, 227])
 
 # TODO: pass placeholder as first argument to `AlexNet`.
-fc7 = AlexNet(..., feature_extract=True)
+fc7 = AlexNet(resized, feature_extract=True)
 # NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
 # past this point, keeping the weights before and up to `fc7` frozen.
 # This also makes training faster, less work to do!
 fc7 = tf.stop_gradient(fc7)
 
 # TODO: Add the final layer for traffic sign classification.
+shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix)
+fc8W = tf.Variable(tf.truncated_normal(shape, stddev=1e-2))
+fc8b = tf.Variable(tf.zeros(nb_classes))
+logits = tf.nn.xw_plus_b(fc7, fc8W, fc8b)
 
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
+loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+opt_op = tf.train.AdamOptimizer()
+training_operation = opt_op.minimize(loss_op)
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # TODO: Train and evaluate the feature extraction model.
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_loss = 0
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        loss, accuracy = sess.run([loss_op, accuracy_operation], feed_dict={features: batch_x, labels: batch_y})
+        total_loss += (loss * len(batch_x))
+        total_accuracy += (accuracy * len(batch_x))
+    return total_loss / num_examples,  total_accuracy / num_examples
+
+
+print("Training...")
+print()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        t0 = time.time()
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={features: batch_x, labels: batch_y})
+
+        val_loss, val_acc = evaluate(X_val, y_val)
+        print()
+        print("EPOCH {} ...".format(i+1))
+        print("Time: %.3f seconds" % (time.time() - t0))
+        print("Validation Accuracy = {:.3f}".format(val_loss))
+        print("Validation Accuracy = {:.3f}".format(val_acc))
+        print()
+
+    saver.save(sess, save_model_path)
